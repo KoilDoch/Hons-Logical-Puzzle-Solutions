f32ef14c8225a5463a1d11a346fbee541e489e33
Kyle <kyled073@gmail.com>
2023-01-29 19:01:07 +0000

    Working on consistencyCheck
    Moving workstation

--- Code/Mastermind/Exhaustive/Exhaustive.hs
+++ Code/Mastermind/Exhaustive/Exhaustive.hs
@@ -2,15 +2,17 @@
 -- Date of Creation: 17/10/2022
 -- Date of Last Edit: 15/01/2023
 -- Author: Kyle Dick, kd41@hw.ac.uk
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Eta reduce" #-}
 
 import Data.List
 import Distribution.Compat.CharParsing (space)
 
-exhaustSol :: ([a], (Int, Int)) -> [([a], (Int,Int))] -> [a]
-exhaustSol guess space = foldl assessPerm [] space
-    where 
-        assessPerm consistent perm
-            | fst guess > snd $ fst perm = consistent : [perm]
+-- solveStep :: ([a], (Int, Int)) -> [([a], (Int,Int))] -> [a]
+-- exhaustSol guess space = foldl assessPerm [] space
+--     where 
+--         assessPerm consistent perm
+--             | fst guess > snd $ fst perm = consistent : [perm]
     
 {-
 This function evaluates a code based on the hidden combination
@@ -26,16 +28,21 @@ checkGuess code guess = foldl assignPegs (0,0) (zip code guess)
         | x `elem` guess = (black + 1, white)
         | otherwise = (black, white)
 
--- takes the code and the list of previous guesses and tries the solution on each
-mastermind :: Eq a => [a] -> [a] -> [([a], (Int,Int))] -> [([a], (Int,Int))]
-mastermind symbols code guesses = 
-    let guess = bruteSolution symbols guesses
-        check = checkGuess code guess
-    in if check == (0,4)
-        then (guess, (0,4)) : guesses
-        else mastermind symbols code ((guess,check) : guesses)
 
-main :: IO()
-main = do 
-    let x = mastermind ['A','B','C','D','E','F'] ['A','C','D','E'] []
-        in print (length x)
+checkConsistency :: Eq a => [a] -> [([a], (Int,Int))] -> [[a]]
+checkConsistency code consistentSet = map (\(list,_) -> map (\x -> x) list) consistentSet
+
+ 
+-- takes the code and the list of previous guesses and tries the solution on each
+-- mastermind :: Eq a => [a] -> [a] -> [([a], (Int,Int))] -> [([a], (Int,Int))]
+-- mastermind symbols code guesses = 
+--     let guess = solveStep symbols guesses
+--         check = checkGuess code guess
+--     in if check == (0,4)
+--         then (guess, (0,4)) : guesses
+--         else mastermind symbols code ((guess,check) : guesses)
+
+-- main :: IO()
+-- main = do 
+--     let x = mastermind ['A','B','C','D','E','F'] ['A','C','D','E'] []
+--         in print (length x)
